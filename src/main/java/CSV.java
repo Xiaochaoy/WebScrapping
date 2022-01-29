28283221f5b2aa715c826e4e9e49c3d32add69c0
Xiaochaoy <chaoshixiaochao@gmail.com>
2022-01-29 23:59:57 +0100

    v4

--- src/main/java/CSV.java
+++ src/main/java/CSV.java
@@ -10,12 +10,14 @@ public class CSV {
     File file = new File("lol.csv");
     public CSV(List<Champion> champions) {
         CSVWriter csvWriter;
+        String[] head = {"Nom","Rol","Historia"};
         List<String[]> data = new ArrayList<>();
         for (Champion c: champions){
             data.add(new String[] {c.nombre,c.rol,c.historia});
         }
         try {
             csvWriter = new CSVWriter(new FileWriter(file));
+            csvWriter.writeNext(head);
             csvWriter.writeAll(data);
             csvWriter.close();
         } catch (IOException e) {
